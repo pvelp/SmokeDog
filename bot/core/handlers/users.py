2ee53349be068073cc30b42c76a51f4d4d88bbbe
pvelp <pve1pg6@gmail.com>
2023-07-30 16:01:13 +0300

    add base registration handlers

--- bot/core/handlers/users.py
+++ bot/core/handlers/users.py
@@ -1,25 +1,46 @@
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
-from core.models import ClientModel
+from bot.core.models import ClientModel
 from base.db_models.models import Client
-from bot.core.db.actions import get_client_by_id, add_new_client
-from bot.core.states.user_state import UserState
+from bot.keyboards.utils import create_buttons
+from bot.keyboards.cancel_keyboard import cancel, cancel_send_phone, CancelBtnName
+from bot.core.db.actions import get_client_by_tg_id, add_new_client
+from bot.core.states.user_state import UserState, Registration
 
 
 async def start_command(message: types.Message):
-    user = get_client_by_id(message.from_user.id)
+    user = get_client_by_tg_id(message.from_user.id)
     if user is None:
-        await message.answer("Пройдите регистрацию")
+        kb = create_buttons(["Зарегистрироваться"])
+        await message.answer("Пройдите регистрацию", reply_markup=kb)
         await UserState.sign_up.set()
     else:
         await message.answer("Рады снова вас видеть")
 
 
-async def start_menu(message: types.Message):
-    await message.answer("Hello world")
+async def sign_up_start_stage(message: types.Message):
+    msg = message.text
+    if msg == "Зарегистрироваться":
+        await message.answer("*Шаг [1/3]*\nВведите вашe имя", reply_markup=cancel(), parse_mode="Markdown")
+        await Registration.name_stage.set()
+    elif msg == CancelBtnName.cancel_btn:
+        await message.answer("Вы отменили регистрацию")
+        await UserState.default_state.set()
+
+
+async def sign_up_name_stage(message: types.Message, ctx: FSMContext):
+    msg = message.text
+    if msg == CancelBtnName.cancel_btn:
+        await message.answer("Вы отменили регистрацию")
+        await UserState.default_state.set()
+    else:
+        await message.answer("*Шаг [2/3]*\nВведите дату вашего рождения")
+        await ctx.update_data(name=msg)
+        await Registration.birthday_stage.set()
 
 
 def register_users_handlers(dp: Dispatcher):
-    dp.register_message_handler(start_command, commands=['start'])
-    dp.register_message_handler(start_menu, state="*")
+    dp.register_message_handler(start_command, commands=['start'], state=["*"])
+    dp.register_message_handler(sign_up_start_stage, state=UserState.sign_up)
+    dp.register_message_handler(sign_up_name_stage, state=Registration.name_stage)
--- bot/core/states/user_state.py
+++ bot/core/states/user_state.py
@@ -2,5 +2,12 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class UserState(StatesGroup):
+    default_state = State()
     start = State()
     sign_up = State()
+
+
+class Registration(StatesGroup):
+    name_stage = State()
+    birthday_stage = State()
+    phone_stage = State()
